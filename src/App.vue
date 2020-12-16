43222b440421f3271138bc0df8acd38230f56f9c
Paulsonbigi <adegbokanpaul@gmail.com>
2020-12-16 16:50:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddTodo/>
+      <FilterTodo/>
+      <Todos/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
+import FilterTodo from "./components/FilterTodo.vue";
+export default {
+  name: "App",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodo
+  }
+};
+</script>
+
+<style>
+body{
+  font-family: "Franklin Gothic Medium", "Arial Namrrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container{
+  max-width: 1100px;
+  margin: auto;
+  overflow: 0 2rem;
+}
+</style>
